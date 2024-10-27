12d71661a8de3cacce3c25ca3bfb8d6f6878eefd
Chakib dibt <123759398+Stazkun01@users.noreply.github.com>
2024-10-27 18:26:04 +0100

    Update main.c

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ struct registration{
 
 };
 int main(){
-    float  fin ,linux , dev , math , win , ar , fr , eeje , com ,result , somme , module1 , module2 , module3 ;
+    float  fin ,linux , dev , math , win , ar , fr , eeje , com , eng ,result , somme , module1 , module2 , module3 ;
     struct registration user;
     struct registrationdates date ;
     printf("entrer votre nom : ");
@@ -28,6 +28,19 @@ int main(){
     scanf("%d" , &date.month);
     printf("entrer le jour de naissance : ");
     scanf("%d" , &date.day);
+    system("cls");
+    printf("                                  LOADING | ");
+    Sleep(200);
+    system("cls");
+    printf("                                  LOADING / ");
+    Sleep(200);
+    system("cls");
+    printf("                                  LOADING - ");
+    Sleep(200);
+    system("cls");
+    printf("                                  LOADING \\ ");
+    Sleep(200);
+    system("cls");
    printf("user registred successfully !");
     Sleep(2500);
     system("cls");
@@ -81,6 +94,12 @@ int main(){
         printf("la note doit etre positive et inferieur a 20 !\n");
         printf("entrer la note de communication : ");
         scanf("%f" , &com);}
+    printf("entrer la note d'anglais : ");
+    scanf("%f" , &eng);
+    while (eng > 20 || eng < 0){
+        printf("la note doit etre positive et inferieur a 20 !\n");
+        printf("entrer la note d'anglais : ");
+        scanf("%f" , &eng);}
     system("cls");
     printf("                                  LOADING | ");
     Sleep(200);
@@ -122,6 +141,7 @@ int main(){
     printf(" Date de naissance : %02d/%02d/%02d \n\n\n" , date.day , date.month , date.year);
     printf(" =================================Bultain==================================\n");
     printf("|  matiere                        |   notes    | coeficient                |   \n");
+    printf("|--------------------------------------------------------------------------|\n");
     printf("|  linux                          |  %.2f/20  |  5                        |\n" , linux);
     printf("|  developpement des applications |  %.2f/20  |  5                        |\n" , dev);
     printf("|  math                           |  %.2f/20  |  3                        |\n" , math);
@@ -130,31 +150,32 @@ int main(){
     printf("|  francais                       |  %.2f/20  |  2                        |\n" , fr);
     printf("|  eeje                           |  %.2f/20  |  3                        |\n" , eeje);
     printf("|  communication                  |  %.2f/20  |  2                        |\n" , com);
+    printf("|  anglais                        |  %.2f/20  |  2                        |\n" , com);
     printf(" ==========================================================================\n");
-    somme = 27;
+    somme = 29;
     module1 = (linux + math) / 2;
-    module2 = (ar + fr + com + eeje) / 4;
+    module2 = (ar + fr + com + eeje + eng) / 5;
     module3 = (dev + win) /2;
-    result = (linux * 5) + (dev * 5) + (math * 3) + (win * 5) + (ar * 2) + (fr * 2) +(eeje * 3) + (com * 2) ;
+    result = (linux * 5) + (dev * 5) + (math * 3) + (win * 5) + (ar * 2) + (fr * 2) +(eeje * 3) + (com * 2) + (eng *2) ;
     fin = result / somme;
-    printf("|               | la note final est : %.2f  |                             |\n" , fin);
-    if (fin < 10 || linux < 6 || dev < 6 || math < 6 || win < 6 || ar < 6 || fr < 6 || eeje < 6 || com < 6  || module1 < 10 || module2 < 10 || module3 < 10){
-         printf("|                  vous n'avais pas reussi !                               |\n");
-         printf("|                 cause : note eliminatoire                                |\n");
+    printf("|                     | la note final est : %.2f  |                       |\n" , fin);
+    if (fin < 10 || linux < 6 || dev < 6 || math < 6 || win < 6 || ar < 6 || fr < 6 || eeje < 6 || com < 6  || eng < 6 || module1 < 10 || module2 < 10 || module3 < 10){
+         printf("|                       vous n'avais pas reussi !                          |\n");
+         printf("|                      cause : note eliminatoire                           |\n");
     }
-    if(fin >=10 && linux > 6 && dev > 6 && math > 6 && win > 6 && ar > 6 && fr > 6 && eeje > 6 && com > 6  && module1 > 10 && module2 > 10 && module3 > 10){
-        printf("|               Mabrouuuuk vous avez reussie  !                            |\n");
+    if(fin >= 10 && linux > 6 && dev > 6 && math > 6 && win > 6 && ar > 6 && fr > 6 && eeje > 6 && com > 6  && eng > 6 && module1 >= 10 && module2 >= 10 && module3 >= 10){
+        printf("|                     Mabrouuuuk vous avez reussie  !                      |\n");
     }
-    if (10<fin && fin<=12 && linux > 6 && dev > 6 && math > 6 && win > 6 && ar > 6 && fr > 6 && eeje > 6 && com > 6  && module1 > 10 && module2 > 10 && module3 > 10){
-        printf("|                       niveau passable                                    |\n");
+    if (10<=fin && fin<12 && linux > 6 && dev > 6 && math > 6 && win > 6 && ar > 6 && fr > 6 && eeje > 6 && com > 6 && eng > 6 && module1 >= 10 && module2 >= 10 && module3 >= 10){
+        printf("|                             niveau passable                              |\n");
     }
-    else if (12<fin && fin <=14 && linux > 6 && dev > 6 && math > 6 && win > 6 && ar > 6 && fr > 6 && eeje > 6 && com > 6  && module1 > 10 && module2 > 10 && module3 > 10){
-        printf("|                       niveau assez bien                                  |\n");
+    else if (12<=fin && fin <14 && linux > 6 && dev > 6 && math > 6 && win > 6 && ar > 6 && fr > 6 && eeje > 6 && com > 6 && eng > 6 && module1 >= 10 && module2 >= 10 && module3 >= 10){
+        printf("|                         niveau assez bien                               |\n");
     }
-    else if (14<fin && fin<=16 && linux > 6 && dev > 6 && math > 6 && win > 6 && ar > 6 && fr > 6 && eeje > 6 && com > 6  && module1 > 10 && module2 > 10 && module3 > 10){
-        printf("|                       niveau bien                                        |\n");
+    else if (14<=fin && fin<16 && linux > 6 && dev > 6 && math > 6 && win > 6 && ar > 6 && fr > 6 && eeje > 6 && com > 6 && eng > 6 && module1 >= 10 && module2 >= 10 && module3 >= 10){
+        printf("|                             niveau bien                                  |\n");
     }
-    else if (16<fin && fin<=20 && linux > 6 && dev > 6 && math > 6 && win > 6 && ar > 6 && fr > 6 && eeje > 6 && com > 6  && module1 > 10 && module2 > 10 && module3 > 10){
+    else if (16<=fin && fin<20 && linux > 6 && dev > 6 && math > 6 && win > 6 && ar > 6 && fr > 6 && eeje > 6 && com > 6 && eng > 6 &&module1 >= 10 && module2 >= 10 && module3 >= 10){
         printf("|                        niveau excellent                                  |\n");
     }
     printf(" ========================================================================== \n");
